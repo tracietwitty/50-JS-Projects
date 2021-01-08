49840b13128d4d45f4448690bd322289dfb17e7c
Tracie <46723642+tracietwitty@users.noreply.github.com>
2021-01-08 12:07:06 -0600

    Day 28: Github Profiles
    This project uses Axios to query the Github API and return a specific profile based on user input.

---
+++ github-profiles/Readme.md
@@ -0,0 +1,31 @@
+# "Github Profiles"
+ 
+
+
+## :page_facing_up: Description
+This project uses Axios to query the Github API and return a specific profile based on user input.
+
+This is Day 28/50 of Brad Traversey's 50 Projects in 50 Days course on [Udemy](https://www.udemy.com/course/50-projects-50-days/learn/lecture/23594652?start=15#content)
+
+> I enjoyed this one a lot because it required pulling outside information into my JS code. I had a lot more errors than I have in previous lessons, but each one taught me the importance of understanding what my code is *actually* doing, instead of just copying what Brad has on his screen.
+
+
+
+## Challenges
+Async/Await
+> It has been awhiiiiiiile since I've used asynchronous programming. I am rusty! I had a deeper look into the documentation on MDN and it helped a ton.
+
+
+## New Tricks
+> Not necessarily *new* but re-introducing myself to async/await felt new enough!
+
+
+## :books: Resources
+* [Async/Promise Review](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function)
+* [icons8](https://icons8.com/)
+* [Google Fonts](https://fonts.google.com/)
+* [Mastering Markdown Guide](https://guides.github.com/features/mastering-markdown/)
+* [CDN JS](https://cdnjs.com/libraries/axios)
+* [Github Endpoints](https://docs.github.com/en/free-pro-team@latest/rest/overview/endpoints-available-for-github-apps)
+
+
---
+++ github-profiles/script.js
@@ -0,0 +1,86 @@
+const APIURL = 'https://api.github.com/users/'
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+async function getUser(username) {
+    try {
+        const {data} = await axios(APIURL + username)
+        createUserCard(data)
+        getRepos(username) 
+    } catch (err) {
+        if(err.response.status == 404){
+            createErrorCard('No profile with this username')
+        }
+    }
+}
+
+function createUserCard(user) {
+    const cardHTML = `
+        <div class="card">
+            <div>
+                <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+            </div>
+            <div class="user-info">
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
+
+                <ul>
+                    <li>${user.followers} <strong>Followers</strong></li>
+                    <li>${user.following} <strong>Following</strong></li>
+                    <li>${user.public_repos} <strong>Repos</strong></li>
+                </ul>
+
+                <div id="repos"></div>
+            </div>
+        </div>
+    `
+
+    main.innerHTML = cardHTML
+}
+
+function createErrorCard(message) {
+    const cardHTML = `
+        <div class="card">
+            <h1>${message}</h1>
+        </div> 
+    `
+
+    main.innerHTML = cardHTML
+}
+
+function addReposToCard(repos) {
+    const reposElement = document.getElementById('repos')
+    repos
+        .slice(0,5)
+        .forEach(repo => {
+            const repoElement = document.createElement('a')
+            repoElement.classList.add('repo')
+            repoElement.href = repo.html_url
+            repoElement.target = '_blank'
+            repoElement.innerText = repo.name
+
+            reposElement.appendChild(repoElement)
+        })
+}
+
+async function getRepos(username) {
+    try {
+        const {data} = await axios(APIURL + username + '/repos?sort=created')
+        addReposToCard(data) 
+    } catch (err) {
+            createErrorCard(`We couldn't fetch those repos`)
+    }
+}
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const user = search.value
+
+    if(user) {
+        getUser(user)
+        search.value = ''
+    }
+})
