a5f31ba9e3e8b86e772c6cdff863ae67cf42798f
Tracie <46723642+tracietwitty@users.noreply.github.com>
2021-01-14 17:17:23 -0600

    Day 32: Sneaky Sliders
    This project uses keyframe animations in CSS and JS logic statements to create an interactive slider

---
+++ sneaky-sliders/script.js
@@ -0,0 +1,24 @@
+const toggles = document.querySelectorAll('.toggle')
+const good = document.querySelector('#good')
+const cheap = document.querySelector('#cheap')
+const fast = document.querySelector('#fast')
+
+toggles.forEach(toggle => toggle.addEventListener('change', (event) => {
+    doTheTrick(event.target)
+}))
+
+function doTheTrick(theClicked) {
+    if(good.checked && cheap.checked && fast.checked) {
+        if(good === theClicked) {
+            fast.checked = false
+        }
+
+        if(cheap === theClicked) {
+            good.checked = false
+        }
+
+        if(fast === theClicked) {
+            cheap.checked = false
+        }
+    }
+}
